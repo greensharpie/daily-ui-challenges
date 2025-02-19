4b2cdd6758e60ad261ea19d12ceda2228d0f4282
Kevin Blanco <kevinjblanco@gmail.com>
2025-02-19 11:08:29 -0500

    feat: textinput component

---
+++ 001/src/components/TextInput.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+interface TextInputProps {
+  name: string;
+  label: string;
+  placeholder?: string;
+  type?: string;
+  required?: boolean;
+  onChange?: () => void;
+}
+
+export const TextInput: React.FC<TextInputProps> = ({ name, label, placeholder, type, required, maxLength, minLength, onChange }) => {
+  return (
+    <div>
+      <label htmlFor={name}>{label}</label>
+      <input
+        id={name}
+        name={name}
+        placeholder={placeholder}
+        type={type}
+        required={required}
+        maxLength={maxLength}
+        minLength={minLength}
+        onChange={onChange}
+      />
+    </div>
+  )
+}
