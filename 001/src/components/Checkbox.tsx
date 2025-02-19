d74b2bbe196a728a9809f7ad247a54b03cff9c71
Kevin Blanco <kevinjblanco@gmail.com>
2025-02-19 14:12:55 -0500

    feat: Checkbox component

---
+++ 001/src/components/Checkbox.tsx
@@ -0,0 +1,29 @@
+import React, { useState } from "react"; 
+import '../styles/CheckBox.css'
+interface CheckboxProps {
+  label: string
+  title: string
+  defaultChecked?: boolean
+  onChange?: (checked: boolean) => void
+}
+
+export const CheckBox: React.FC<CheckboxProps> = ({ label, title, defaultChecked = false, onChange }) => {
+  const [checked, setChecked] = useState(defaultChecked)
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setChecked(event.target.checked)
+    if(onChange){
+      onChange && onChange(event.target.checked)
+    }
+  }
+  return (
+    <label className="checkbox-container">
+      <input 
+      type="checkbox"
+      checked={checked}
+      onChange={handleChange}
+      aria-label={label}
+      />
+      <span>{title}</span>
+    </label>
+  )
+}
